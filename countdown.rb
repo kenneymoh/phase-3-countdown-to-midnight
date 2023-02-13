cac9482327207ecf74743a3230445a6b5ca6466d
kennedy <kenneyrit@gmail.com>
2023-02-13 06:30:10 +0300

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
-end
+def countdown int
+  int = 10
+      while int >= 1
+          puts "#{int} SECOND(S)!"
+          # sleep(0.5)
+      int -=1
+      end
+      "HAPPY NEW YEAR!"
+  end
+  # countdown(10)
+
+  def countdown_with_sleep int
+      int = 10
+      while int >= 1
+          sleep 0.5
+          puts "#{int} SECOND(S)!"
+      int -=1
+      end
+      "HAPPY NEW YEAR!"
+  end
